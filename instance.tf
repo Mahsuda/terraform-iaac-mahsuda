544ac3d810178b0f8084986566ae109547ba7703
Mahsuda <maqsudat@yahoo.com>
2019-12-18 20:04:48 -0600

    addd +1

--- instance.tf
+++ instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   user_data = file("userdata_file")
   security_groups = ["allow_ssh"] 
 tags = { 
-    Name = "HelloWorld${count.index}"
+    Name = "HelloWorld${count.index +1}"
   } 
 } 
 
