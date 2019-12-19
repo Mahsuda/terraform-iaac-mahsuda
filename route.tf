5cdd38b9f99fee8026a55336087ef1d18c1de4aa
Mahsuda <maqsudat@yahoo.com>
2019-12-18 19:20:55 -0600

    route53

---
+++ route.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" { 
+  zone_id = "Z180QC6O3SYROP"
+  name    = "www.example.com" 
+  type    = "A" 
+  ttl     = "60" 
+  records = [aws_instance.web.public_ip] 
+} 
