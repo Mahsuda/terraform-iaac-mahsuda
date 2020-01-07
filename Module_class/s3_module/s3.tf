3f491088e86909a08a9423a4af763ce037602841
Mahsuda <maqsudat@yahoo.com>
2020-01-06 18:41:58 -0600

    added s3

--- Module_class/s3_module/s3.tf
+++ Module_class/s3_module/s3.tf
@@ -1,12 +1,8 @@
-module "bucket1" {
-
-  source = "./module"
-
-  bucketname = "some-made-up-bucket-farrukh"
-
-}
-resource "aws_s3_bucket" "b" {
-
-  bucket = var.bucketname
-
+provider "aws"{ 
+  region = "us-east-1" 
+} 
+ 
+module "bucket1" { 
+  source = "./modules" 
+  bucketname = "some-made-up-bucket-ulan-devops" 
 }
