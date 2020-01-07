8069c00d858b1211c7356702c97bb002fd0b6d06
Mahsuda <maqsudat@yahoo.com>
2020-01-06 20:24:51 -0600

    fixed

--- Application/infrastructure_wordpress.tf
+++ Application/infrastructure_wordpress.tf
@@ -2,7 +2,7 @@ module "wordpress1" {
   source = "terraform-aws-modules/vpc/aws"
   name = "wordpress1"
   cidr = "var.cidr"
-  azs             = ["es-east-1a", "es-east-1b", "es-east-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway = true
