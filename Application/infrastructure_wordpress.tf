356ccb61e738b1f6ecbb87bd108fa7b3271f310e
Mahsuda <maqsudat@yahoo.com>
2020-01-06 20:23:45 -0600

    added region

--- Application/infrastructure_wordpress.tf
+++ Application/infrastructure_wordpress.tf
@@ -2,7 +2,7 @@ module "wordpress1" {
   source = "terraform-aws-modules/vpc/aws"
   name = "wordpress1"
   cidr = "var.cidr"
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["es-east-1a", "es-east-1b", "es-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway = true
