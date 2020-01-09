61a4183093b181bddd7f6b7a500bf450f1505e67
Mahsuda <maqsudat@yahoo.com>
2020-01-08 19:53:58 -0600

    fixed

--- data_source/tower.tf
+++ data_source/tower.tf
@@ -6,16 +6,16 @@ provider "aws" {
 data "aws_ami" "ubuntu" {
     most_recent = true
     owners = ["099720109477"]
-     filter { 
-        name  = "root-device-type" 
-         values = ["ebs"] 
+     filter {
+        name  = "root-device-type"
+         values = ["ebs"]
     most_recent =true
     owners = [679593333241]
-         } 
-     filter { 
-         name ="name" 
-         values = ["CentOS Linux 7 x86_64 HVM EBS *"] 
-         } 
+         }
+     filter {
+         name ="name"
+         values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+         }
          }
 
 # Show  AMI id
@@ -23,19 +23,19 @@ output "ami" {
     value = "${data_ami_centos_id}"
 }
 
-resource "aws_key_pair" "${towerkey}" { 
-  key_name   = "${towerkey}" 
-  public_key = "${file("${var.key_name_location}")}" 
+resource "aws_key_pair" "${towerkey}" {
+  key_name   = "${towerkey}"
+  public_key = "${file("${var.key_name_location}")}"
 } 
 
- provisioner "remote-exec" { 
+ provisioner "remote-exec" {
      connection {
           host = "${self.public_ip}"
           type = "ssh"
           user = "${var.user}"
           private_key = "${file("~/.ssh/id_rsa")}"
           inline = [
-              "sudo yum install -y epel-release", 
+              "sudo yum install -y epel-release",
               ]
               }
               }
@@ -48,10 +48,10 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_route53_record" "tower" { 
-  zone_id = "Z180QC6O3SYROP" 
-  name    = "tower.example.com" 
-  type    = "A" 
-  ttl     = "300" 
-  records = ["aws_instance.web.public_ip] 
-  }
+resource "aws_route53_record" "tower" {
+  zone_id = "Z180QC6O3SYROP"
+  name    = "tower.example.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["aws_instance.web.public_ip]
+}
