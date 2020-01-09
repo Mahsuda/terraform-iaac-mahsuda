d1e1a4003f076dae2e021b03d9ebf23809d590f9
Mahsuda <maqsudat@yahoo.com>
2020-01-08 18:37:13 -0600

    added data source

---
+++ data_source/ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+}
+# Search for Ubuntu Latest with the owner
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+}
+
+# Show  owner ID 
+output "ami" {
+  value = "${data.aws_ami.ubuntu.id}"
+}
