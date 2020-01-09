4f14a41dc9d57dca6493a4b25a053acc66fd9da5
Mahsuda <maqsudat@yahoo.com>
2020-01-08 18:39:30 -0600

    fixed

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-2"
+    region = "us-east-2"
 }
 # Search for Ubuntu Latest with the owner
 data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["099720109477"]
+    most_recent = true
+    owners = ["099720109477"]
 }
 
 # Show  owner ID 
 output "ami" {
-  value = "${data.aws_ami.ubuntu.id}"
+    value = "${data.aws_ami.ubuntu.id}"
 }
