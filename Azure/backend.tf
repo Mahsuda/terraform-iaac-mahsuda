aedd9398b182853242cd833cda928fcd84b7b8bf
Mahsuda <maqsudat@yahoo.com>
2020-01-18 12:51:40 -0600

    fixed

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "mahsudabackend"
-    storage_account_name = "mahsudastorage"
+    resource_group_name  = "mahsudastorage"
+    storage_account_name = "mahsudabackend"
     container_name       = "tfstate"
     key                  = "prod.terraform.state"
   }
