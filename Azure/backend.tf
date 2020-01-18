0820076efe5e3bea3a127e03dc1855209b3d2986
Mahsuda <maqsudat@yahoo.com>
2020-01-18 13:03:26 -0600

    added access key

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "mahsudabackend"
     container_name       = "tfstate"
     key                  = "prod.terraform.state"
-    access_key           = ""
+    access_key           = "J2xAoRroQC4kRK++fNxXpQ7BCR/yzebjjTT+/LqKe2fFX0sZtEXS5KXSwnfXrvs0Jd0F6AggZN7CkrirxvYBag=="
   }
 }
